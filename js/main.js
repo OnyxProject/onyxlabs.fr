bc143c2bd642423af3cfe48816446c7605c30345
Aituglo <ckhouani@live.fr>
2017-08-09 22:25:00 +0200

    Add contact

--- js/main.js
+++ js/main.js
@@ -1,3 +1,44 @@
+function validateEmail(email){
+    var re = /[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}/igm;
+    return re.test(email);
+}
+
+$(function(){
+
+    $("#contactButton").click(function(){
+
+
+        var msg = {
+            name: $('#name').val(),
+            email: $('#email').val(),
+						subject: $('#subject').val(),
+            description: $('#message').val()
+        };
+
+        if(!validateEmail(msg.email)){
+            Materialize.toast('Invalid email !', 3000, 'rounded', 'red');
+						return ;
+        }
+
+        $.ajax({
+            url: 'https://eez0hajdge.execute-api.us-east-1.amazonaws.com/prod/contact',
+            type: 'POST',
+            data: JSON.stringify(msg),
+            contentType: 'application/json'
+        })
+        .done(function(data){
+						Materialize.toast('Your email has been Sent !', 3000, 'rounded', 'green')
+            $('#message').val("");
+
+        })
+        .fail(function(){
+            Materialize.toast('An Error has occured !', 3000, 'rounded', 'red')
+        });
+        return false;
+    });
+
+});
+
 $(document).ready(function(){
 
 	$("#portfolio-contant-active").mixItUp();
